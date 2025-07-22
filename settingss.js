4135913ef61cbb318160681dad15b5407500015a
ayomide841 <ovars2009@gmail.com>
2025-07-22 10:29:00 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: " ",  // malvin~Nu1XFBrQ#0Mt9Vt31iCtX3bRxPPTlWOx-WWGQrTVclqzEADdrKn0
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
